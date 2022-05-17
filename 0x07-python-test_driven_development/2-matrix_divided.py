51c78802345ddae269a16e84d007a4285d845e78
ntwari-tech <ntwari.yannick@gmail.com>
2022-05-17 17:08:16 +0000

    2main file

---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+""" divides by div """
+
+
+def matrix_divided(matrix, div):
+    """
+    -matrix must be a list/int/float
+    -each row must be the same size
+    -div must be int/float
+    -div can't be 0
+    -round elements by 2
+    -return new matrix
+    """
+    # check div type and isn't 0
+    if not isinstance(div, (int, float)):
+        raise TypeError("div must be a number")
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+
+    # check matrix type
+    if not isinstance(matrix, list):
+        raise TypeError("matrix must be a matrix (list of lists) of\
+         integers/floats")
+
+    # check row type then row length
+    row_length = len(matrix[0])
+    for row in matrix:
+        if not isinstance(row, (int, float, list)):
+            raise TypeError("matrix must be a matrix (list of lists) of\
+             integers/floats")
+    for row in range(len(matrix)):
+        if row_length != len(matrix[row]):
+            raise TypeError("Each row of the matrix must have the same size")
+    divided = [[round(element / div, 2) for element in row] for row in matrix]
+    return divided
