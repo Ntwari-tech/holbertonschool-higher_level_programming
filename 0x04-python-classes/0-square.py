719ba3c9f4dd20faa0f3c6d9b833e2af8dc7c623
ntwari-tech <ntwari.yannick@gmail.com>
2022-05-18 20:26:57 +0000

    defines square

--- 0x04-python-classes/0-square.py
+++ 0x04-python-classes/0-square.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-""" a  module containing a def for a square"""
+"""creates a class is for a square"""
 
 
 class Square:
+    """Here it is. Here's the square."""
+    def __init__(self, size=0):
+        self.__size = size
 
-    """ Square class """
-
-    pass
