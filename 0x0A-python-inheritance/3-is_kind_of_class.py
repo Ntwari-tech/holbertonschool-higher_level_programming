38bfb82b15a3894a8d1e3396e8e6d7d9703c41b5
ntwari-tech <ntwari.yannick@gmail.com>
2022-05-31 18:58:23 +0000

    tru/false instane of a class

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" Python Inheritance """
+
+
+def is_kind_of_class(obj, a_class):
+    """
+    Return True if the object is an instance of,
+    or if the object is an instance
+    """
+    return isinstance(obj, a_class)
