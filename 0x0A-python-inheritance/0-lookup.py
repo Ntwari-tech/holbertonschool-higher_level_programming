170fb0a8b459f0ffa0098181776b00dc944f1a3d
ntwari-tech <ntwari.yannick@gmail.com>
2022-05-31 16:27:14 +0000

    inheritance

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" Lookup class """
+
+
+def lookup(obj):
+    """ Get attribute and methods from an object
+        Args:
+            obj: object to get attributes and methods from
+        Returns:
+            list[str]: list of attributes and methods
+    """
+    return dir(obj)
