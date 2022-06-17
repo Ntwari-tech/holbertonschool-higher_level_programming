219596a2ed70835b944c06d333e0532de553b8e3
Yannick Ntwari <yannickntwari@Yannicks-MacBook-Pro.local>
2022-06-17 18:15:06 -0500

    upgraded

---
+++ 0x01-SQL_more_queries/6-states.sql
@@ -0,0 +1,8 @@
+-- this scripts creates database and a table 
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states(
+    id INT NOT NULL AUTO_INCREAMENT,
+    name VARCHAR(256) NOT NULL,
+    PRIMARY KEY (id)
+);
