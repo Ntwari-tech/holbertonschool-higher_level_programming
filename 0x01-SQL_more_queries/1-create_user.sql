30d728cac235a15daea4d61862adf18f223b869e
Yannick Ntwari <yannickntwari@Yannicks-MacBook-Pro.local>
2022-06-17 16:04:09 -0500

    updates

--- 0x01-SQL_more_queries/1-create_user.sql
+++ 0x01-SQL_more_queries/1-create_user.sql
@@ -1,4 +1,4 @@
--- This script creates MYSQL server user
-CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost'
+-- this script creates MYSQL server for a user
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
+IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVELEGES ON * . * TO 'user_0d_1'@'localhost';
