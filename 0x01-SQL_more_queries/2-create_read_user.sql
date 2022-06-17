10b93553a6544ad7909552af0d56f05afcf47e24
Yannick Ntwari <yannickntwari@Yannicks-MacBook-Pro.local>
2022-06-17 16:25:29 -0500

    create a user

---
+++ 0x01-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,5 @@
+-- this script creates a database and the user
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost'
+IDENTIFIED BY 'user_0d_pwd';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
