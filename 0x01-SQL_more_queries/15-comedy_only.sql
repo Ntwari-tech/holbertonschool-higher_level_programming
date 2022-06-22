8562e31618e88edf3bbf4ded45381ac623663906
Yannick Ntwari <yannickntwari@Yannicks-MacBook-Pro.local>
2022-06-22 13:42:03 -0500

    changed

--- 0x01-SQL_more_queries/15-comedy_only.sql
+++ 0x01-SQL_more_queries/15-comedy_only.sql
@@ -1,5 +1,5 @@
 -- this script lists all the comdedy shows in a given database
-SELECT tv_shows.title from tv_shows
+SELECT tv_shows.title FROM tv_shows
 JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
 WHERE tv_genres.name = 'Comedy'
