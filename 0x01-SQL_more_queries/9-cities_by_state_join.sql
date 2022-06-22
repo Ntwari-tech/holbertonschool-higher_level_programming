60ada22e28ac980b2a55356df105ea2315ebeefa
Yannick Ntwari <yannickntwari@Yannicks-MacBook-Pro.local>
2022-06-22 12:46:03 -0500

    list all the cities

--- 0x01-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x01-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,4 @@
 -- this scripts lists all the cities found in the server
-SELECT cities.id, cities.name, states.name
-FROM cities
-JOIN states on cities.state_id = state_id
+SELECT cities.id, cities.name, states.name FROM cities
+JOIN states ON cities.state_id = state.id
 ORDER BY cities.id ASC;
