6d35c6452b39f0520ecd965bdd74bd05638d888d
Yannick Ntwari <yannickntwari@Yannicks-MacBook-Pro.local>
2022-06-22 13:04:56 -0500

    updates

--- 0x01-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x01-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,5 +1,4 @@
 -- this script list all the shows found in a given server
-SELECT tv_shows.titl, tv_show_genres.genre_id
-FROM tv_shows
-LEFT JOIN tv_show_genres ON tv_show_genres.show_id
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 ORDER BY tv_shows.title, tv_show_genres.genre_id;
