14d22e73fd26943fbddc0342753ff26f6eb73a0f
Yannick Ntwari <yannickntwari@Yannicks-MacBook-Pro.local>
2022-06-22 13:17:45 -0500

    updates

--- 0x01-SQL_more_queries/12-no_genre.sql
+++ 0x01-SQL_more_queries/12-no_genre.sql
@@ -1,6 +1,5 @@
 -- this script list all the shows without genre
-SELECT tv_shows.title, tv_show_genres.genre_id
-FROM tv_shows 
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows
 LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 WHERE tv_show_genres.genre_id is NULL
 ORDER BY tv_shows.tittle, tv_show_genres.genre_id;
