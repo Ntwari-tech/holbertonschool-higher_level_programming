0f5fb953d15e07ef7243fb71c3f3b3437e8d8aae
Ntwari-tech <ntwari.yannick@gmail.com>
2022-07-11 13:21:33 -0400

    updated

--- 0x0C-python-network_1/10-my_github.py
+++ 0x0C-python-network_1/10-my_github.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 """takes github credentials and displays ID"""
+
 import requests
 import sys
 from requests.auth import HTTPBasicAuth
+
+
 if __name__ == "__main__":
     url = 'https://api.github.com/user'
     g = requests.get('https://api.github.com/user',
