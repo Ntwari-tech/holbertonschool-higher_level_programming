f4848b927f5dceeb00526044a010d4ff80234be4
Ntwari-tech <ntwari.yannick@gmail.com>
2022-07-11 13:06:47 -0400

    updated my code

--- 0x0C-python-network_1/4-hbtn_status.py
+++ 0x0C-python-network_1/4-hbtn_status.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-"""fetches URL using requests"""
-if __name__ == '__main__':
-    import requests
-
-    r = requests.get('https://intranet.hbtn.io/status')
-    print(f"Body response:\n\t- type: {type(r.text)}\n\t- content: {r.text}")
+"""fetche  https://intranet.hbtn.io/status"""
+import requests
+r = requests.get('https://intranet.hbtn.io/status')
+print("Body response:")
+print("\t- type: {}".format(type(r.text)))
+print("\t- content: {}".format(r.text))
