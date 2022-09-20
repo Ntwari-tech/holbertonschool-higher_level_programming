5644c986c6647df0f4c80d0525573c150ba5ccb8
Ntwari-tech <ntwari.yannick@gmail.com>
2022-09-19 22:29:39 -0500

    updated commit

--- 0x0C-python-network_1/0-hbtn_status.py
+++ 0x0C-python-network_1/0-hbtn_status.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+from re import U
 import urllib.request
 """ website fetcher """
 with urllib.request.urlopen('https://intranet.hbtn.io/status/') as r:
@@ -7,3 +8,13 @@ with urllib.request.urlopen('https://intranet.hbtn.io/status/') as r:
     print("\t- type: {}".format(type(html)))
     print("\t- content: {}".format(html))
     print("\t- utf8 content: {}".format(html.decode('utf-8')))
+
+
+#!/usr/bin/python3
+import urllib.request
+"""this code is a web fetcher"""
+html = r.read()
+print("body response:")
+print("\t- type: {}".format(type(html)))
+print("\t- content: {}".format(html))
+print("\t- utf8 content: {}".format(html.decod('utf-8')))
---
+++ yannick python/alltrick.py
@@ -0,0 +1,35 @@
+# #!/usr/local/bin/python3:
+# a, b = 0, 1
+# while b < 15:
+# print(b)
+# a, b = b, a+b
+#open_string = "Sammy loves {}."
+#print(open_string.format("open source"))
+
+open_string = "Sammy loves {} {}."
+print(open_string.format("open source", "this software"))
+
+#open_string2 = "jacs loves  {} {} and {} and {} "
+#print(open_string2.format("naps", "food", "these nuts", " i love it" ))
+
+# print("jacs loves {}, {}, {}, {}"). format("naps", "food", dk = " these nuts", "and i love it ")
+# print("this girl is {it},{1}, {2}, {4}".format(it = "intense", "unappreciative", "crazy").
+print("next time jacs sees this old while man {0:d} % {1}".format(
+    99, "gonna be sight"))
+using(start iterator)
+new_jacs = "pregnant with  {} baby "
+3	baby = 1
+4 for i in range(6):
+5 print(new_jacs.format(baby))
+
+
+#using (start, stop, step)
+for i in range(15, 30, 5):
+    print(new_jacs.format(baby))
+
+# using list[ "+, *" operator with "=" compound form and delete]
+ntwari_fam = ["Andy", "serge", "gaju", "igor", "ana", "kev"]
+for y in range(1, 15):
+    ntwari_fam += ['patrick']  # ntwari_fam + ['patrick'],
+    del ntwari_fam[5]
+    print(ntwari_fam)
